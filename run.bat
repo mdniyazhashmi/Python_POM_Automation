ea1eb90a00e52e0371764901abeb68e5c31903c8
mdniyazhashmi <67937819+mdniyazhashmi@users.noreply.github.com>
2020-08-19 12:49:37 +0530

    updated batch command

--- run.bat
+++ run.bat
@@ -1,5 +1,4 @@
-taskkill /F /IM chromedriver.exe
-REM pytest -v -s -m "sanity" --html=./reports/report_chrome.html testCases/ --browser chrome
+pytest -v -s -m "sanity" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity" --html=./reports/report_firefox.html testCases/ --browser firefox
 
 REM pytest -v -s -m "sanity and regression" --html=./reports/report_chrome.html testCases/ --browser chrome
@@ -8,5 +7,5 @@ REM pytest -v -s -m "sanity and regression" --html=./reports/report_firefox.html
 REM pytest -v -s -m "sanity or regression" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity or regression" --html=./reports/report_firefox.html testCases/ --browser firefox
 
-pytest -v -s -m "regression" --html=./reports/report_chrome.html testCases/ --browser chrome
-pytest -v -s -m "regression" --html=./reports/report_firefox.html testCases/ --browser firefox
+REM pytest -v -s -m "regression" --html=./reports/report_chrome.html testCases/ --browser chrome
+REM pytest -v -s -m "regression" --html=./reports/report_firefox.html testCases/ --browser firefox
