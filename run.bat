a881081094825d7efc812aef7ebec89fe109921c
unknown <niyaz.hashmi@gmail.com>
2020-08-21 10:55:27 +0530

    batch file update

--- run.bat
+++ run.bat
@@ -1,8 +1,5 @@
-<<<<<<< HEAD
-pytest -v -s -m "sanity" --html=./reports/report_chrome.html testCases/ --browser chrome
-=======
-REM pytest -v -s -m "sanity" --html=./reports/report_chrome.html testCases/ --browser chrome
->>>>>>> 35d35cf76e593e7c23f8cbb759aaede55042d3f2
+
+REMpytest -v -s -m "sanity" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity" --html=./reports/report_firefox.html testCases/ --browser firefox
 
 REM pytest -v -s -m "sanity and regression" --html=./reports/report_chrome.html testCases/ --browser chrome
@@ -11,10 +8,9 @@ REM pytest -v -s -m "sanity and regression" --html=./reports/report_firefox.html
 REM pytest -v -s -m "sanity or regression" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity or regression" --html=./reports/report_firefox.html testCases/ --browser firefox
 
-<<<<<<< HEAD
+
 REM pytest -v -s -m "regression" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "regression" --html=./reports/report_firefox.html testCases/ --browser firefox
-=======
-pytest -v -s -m "regression" --html=./reports/report_chrome.html testCases/ --browser chrome
-REM pytest -v -s -m "regression" --html=./reports/report_firefox.html testCases/ --browser firefox
->>>>>>> 35d35cf76e593e7c23f8cbb759aaede55042d3f2
+
+
+
