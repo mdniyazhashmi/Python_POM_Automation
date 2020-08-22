acfcd6aa91a1b6823f9f40b5fc8f58a38d658216
unknown <niyaz.hashmi@gmail.com>
2020-08-22 21:20:00 +0530

    updated run batch file

--- run.bat
+++ run.bat
@@ -2,14 +2,14 @@
 REM pytest -v -s -m "sanity" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity" --html=./reports/report_firefox.html testCases/ --browser firefox
 
-pytest -v -s -m "sanity and regression" --html=./reports/report_chrome.html testCases/ --browser chrome
+REM pytest -v -s -m "sanity and regression" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity and regression" --html=./reports/report_firefox.html testCases/ --browser firefox
 
 REM pytest -v -s -m "sanity or regression" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "sanity or regression" --html=./reports/report_firefox.html testCases/ --browser firefox
 
 
-REM pytest -v -s -m "regression" --html=./reports/report_chrome.html testCases/ --browser chrome
+pytest -v -s -m "regression" --html=./reports/report_chrome.html testCases/ --browser chrome
 REM pytest -v -s -m "regression" --html=./reports/report_firefox.html testCases/ --browser firefox
 
 
